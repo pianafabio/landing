7d3345b4e1958fbd469d70e08d42d4f223b27a9d
pianafabio <vagrant@homestead>
2020-10-28 13:18:18 +0000

    blocco 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,17 @@
         <title>Landing | Piana Fabio</title>
     </head>
     <body>
-      <div id="blocco_1">
-        <img src="{{ asset('images/Servizio/Group.jpg') }}">
-      </div>
-      <div id="user">
-      </div>
-
-      <!-- React JS -->
-      <script src="{{ asset('js/app.js') }}" defer></script>
-
+        <h1 class="left-heading">Landing Page</h1>
+        <div id="blocco_1">
+            <img src="{{ asset('images/Servizio/Group.jpg') }}" class="blocco_1_image">
+            <div class="blocco_1_text">
+                <h2>Sezione 1 lorem ipsum dolor sit amet.</h2>
+                <h3>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</h3>
+                <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                <a href="#">Scopri di più</a>
+            </div>
+        </div>
+        <!-- React JS -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
