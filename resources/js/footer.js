59e6da6772117ce1f773a3e299ad0b0b583c983c
pianafabio <Fabio@Air-di-Fabio.fritz.box>
2020-11-03 21:07:21 +0100

    fine front end

--- resources/js/footer.js
+++ resources/js/footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import logo_footer from './../images/logo_footer.jpg';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
 
 class Footer extends React.Component{
     render() {
@@ -12,7 +17,7 @@ class Footer extends React.Component{
                     <div className="container">
                         <div className="row">
                             <div className="col-12 text-center">
-                                <h2><strong>Excepteur sint occaecat cupidatat non proident.</strong>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</h2>
+                                <h2><strong>Excepteur sint occaecat cupidatat non proident.</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</h2>
                                 <div className="text-center">
                                     <button className="richiedi_info">
                                         Richiedi Info
@@ -32,17 +37,20 @@ class Footer extends React.Component{
                                     <a href="#">Sit amet</a>
                                 </div>
                                 <div className="footer_social">
-                                <a href="#"><FontAwesomeIcon icon={['fab', 'fa-facebook-square']} /></a>
-                                <a href="#">T</a>
-                                <a href="#">G</a>
-                                <a href="#">Y</a>
+                                <a href="#"><FontAwesomeIcon icon={['fab', 'facebook-square']} /></a>
+                                <a href="#"><FontAwesomeIcon icon={['fab', 'twitter-square']} /></a>
+                                <a href="#"><FontAwesomeIcon icon={['fab', 'google-plus-square']} /></a>
+                                <a href="#"><FontAwesomeIcon icon={['fab', 'youtube-square']} /></a>
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
+                        <div className="row footer_distance">
                             <div className="col-lg-10 offset-lg-1 d-lg-flex justify-content-between align-items-center">
-                                <div className="footer_info">
-                                    Logo
+                                <div className="footer_info d-flex justify-content-left align-items-center footer_logo">
+                                    <img src={logo_footer} />
+                                    <div>
+                                        @Ipsum dolor 2020<br></br>xxx xxxxxxx xx
+                                    </div>
                                 </div>
                                 <div className="footer_breadcrumbs">
                                     Ipsum dolor / Sit amet / Consecteur
