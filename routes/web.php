aca1883daad263018a4624bbcefc26780f6b8292
pianafabio <pianafabio@gmail.com>
2020-11-04 10:18:44 +0100

    create export randomizer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Resources\Randomizer;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/randomizer', function () {
+    return new Randomizer( null );
 });
